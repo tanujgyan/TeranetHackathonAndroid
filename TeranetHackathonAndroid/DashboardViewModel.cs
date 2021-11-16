21943feb1f53cb899685c10373746f9d1a449e51
Tanuj Gyan <35070135+tanujgyan@users.noreply.github.com>
2021-11-15 19:47:55 -0500

    graphs added

---
+++ TeranetHackathonAndroid/DashboardViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TeranetHackathonAndroid
+{
+    public class DashboardViewModel
+    {
+        public List<KeyValue> TotalTransactionByTime { get; set; }
+
+
+        public int TotalRegistrations { get; set; }
+
+        public int TotalSearchs { get; set; }
+
+
+        public List<KeyValue> TotalSearchsByProvince { get; set; }
+
+
+        public List<KeyValue> TotalRegistrationsByProvince { get; set; }
+
+
+        public List<PendingTxnByDate> PendingTransactionByDate { get; set; }
+
+
+        public List<KeyValue> TotalRegistrationsByServiceType { get; set; }
+
+
+        public List<KeyValue> TotalSearchesByServiceType { get; set; }
+    }
+    public class KeyValue
+    {
+        public string Count { get; set; }
+        public string Key { get; set; }
+
+    }
+    public class PendingTxnByDate
+    {
+        public string Date { get; set; }
+        public string ServiceType { get; set; }
+        public string TxnCount { get; set; }
+    }
+}
--- TeranetHackathonAndroid/MainActivity.cs
+++ TeranetHackathonAndroid/MainActivity.cs
@@ -13,72 +13,171 @@ using System.Threading.Tasks;
 using System;
 using System.Net.Http;
 using Newtonsoft.Json;
-
+using OxyPlot.Xamarin.Android;
 namespace TeranetHackathonAndroid
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         ChartView chartView;
+        ChartView chartView2;
+        ChartView chartView3;
+        ChartView chartView4;
+        ChartView chartView5;
         HttpClient client = new HttpClient();
-        List<ChartModelResponse> orderTypeData = new List<ChartModelResponse>();
+        public DashboardViewModel InitialData { get; set; } = new DashboardViewModel();
         protected override void OnCreate(Bundle savedInstanceState)
         {
             
                                    
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            client.BaseAddress = new Uri("https://teranethackathoncosmos.azurewebsites.net/api/Orders/");
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
             HubConnection connection = new HubConnectionBuilder()
-                   .WithUrl("https://azurefunctionsdemo20211105203829.azurewebsites.net/api")
+                   .WithUrl("https://smartcookie.azurewebsites.net/api")
                    .Build();
             connection.StartAsync();
+            GetDataInitial();
             connection.Closed += async (error) =>
             {
                 await Task.Delay(new Random().Next(0, 5) * 1000);
                 await connection.StartAsync();
             };
-            GetOrderType();
-            connection.On<string>("transferchartdata", async (value) => {
-                await GetOrderType();
+            connection.On<string>("target2",  (value) => {
+                InitialData = JsonConvert.DeserializeObject<DashboardViewModel>(value);
+                DrawChartForTotalRegByProvince();
+                DrawChartForTotalSearchByProvince();
+                DrawChartForTotalRegistrationByServiceType();
+                DrawChartForTotalSearchesByServiceType();
+                DrawChartForTransactionsPending();
             });
             chartView = (ChartView)FindViewById(Resource.Id.chartView1);
-           
+            chartView2 = (ChartView)FindViewById(Resource.Id.chartView2);
+            chartView3 = (ChartView)FindViewById(Resource.Id.chartView3);
+            chartView4 = (ChartView)FindViewById(Resource.Id.chartView4);
+            chartView5 = (ChartView)FindViewById(Resource.Id.chartView5);
+
         }
-        public async Task GetOrderType()
+        public async Task GetDataInitial()
         {
             try
             {
-                var response = await client.GetStringAsync("GetOrderTypeChart");
-                orderTypeData = (List<ChartModelResponse>)JsonConvert.DeserializeObject(response, typeof(List<ChartModelResponse>));
-                DrawChart();
+                client.BaseAddress = new Uri("https://smartcookie.azurewebsites.net/api/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var response = await client.GetStringAsync("DashboardLoad2");
+                InitialData = JsonConvert.DeserializeObject<DashboardViewModel>(response);
+                DrawChartForTotalRegByProvince();
+                DrawChartForTotalSearchByProvince();
+                DrawChartForTotalRegistrationByServiceType();
+                DrawChartForTotalSearchesByServiceType();
+                DrawChartForTransactionsPending();
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
         }
-        public void DrawChart()
+
+        private void DrawChartForTotalSearchByProvince()
         {
             List<ChartEntry> entries = new List<ChartEntry>();
-            foreach(var d in orderTypeData)
+           
+            foreach (var d in InitialData.TotalSearchsByProvince)
             {
-                entries.Add(new ChartEntry(d.Data[0])
+                var random = new Random();
+                var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+                entries.Add(new ChartEntry(Convert.ToInt64(d.Count))
                 {
-                    Label = d.Label,
-                    ValueLabel = d.Data[0].ToString(),
-                    Color = SKColor.Parse("#266489")
+                    Label = d.Key,
+                    ValueLabel = d.Count.ToString(),
+                    Color = SKColor.Parse(color)
                 });
             }
-           
-            var barchart = new BarChart { Entries = entries,LabelTextSize=30f };
+
+            var barchart = new BarChart { Entries = entries, LabelTextSize = 30f };
+            chartView2.Chart = barchart;
+        }
+        private void DrawChartForTotalRegistrationByServiceType()
+        {
+            List<ChartEntry> entries = new List<ChartEntry>();
+            foreach (var d in InitialData.TotalRegistrationsByServiceType)
+            {
+                var random = new Random();
+                var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+                entries.Add(new ChartEntry(Convert.ToInt64(d.Count))
+                {
+                    Label = d.Key,
+                    ValueLabel = d.Count.ToString(),
+                    Color = SKColor.Parse(color),
+                    ValueLabelColor = SKColor.Parse(color)
+                });
+            }
+
+            var pieChart = new PieChart { Entries = entries, LabelTextSize= 30f };
+            chartView3.Chart = pieChart;
+        }
+
+        private void DrawChartForTotalSearchesByServiceType()
+        {
+            List<ChartEntry> entries = new List<ChartEntry>();
+            foreach (var d in InitialData.TotalSearchesByServiceType)
+            {
+                var random = new Random();
+                var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+                entries.Add(new ChartEntry(Convert.ToInt64(d.Count))
+                {
+                    Label = d.Key,
+                    ValueLabel = d.Count.ToString(),
+                    Color = SKColor.Parse(color),
+                    ValueLabelColor = SKColor.Parse(color)
+                });
+            }
+
+            var pieChart = new PieChart { Entries = entries, LabelTextSize = 30f };
+            chartView4.Chart = pieChart;
+        }
+
+        private void DrawChartForTotalRegByProvince()
+        {
+            List<ChartEntry> entries = new List<ChartEntry>();
+            foreach (var d in InitialData.TotalRegistrationsByProvince)
+            {
+                var random = new Random();
+                var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+                entries.Add(new ChartEntry(Convert.ToInt64(d.Count))
+                {
+                    Label = d.Key,
+                    ValueLabel = d.Count.ToString(),
+                    Color = SKColor.Parse(color)
+                });
+            }
+
+            var barchart = new BarChart { Entries = entries, LabelTextSize = 30f };
             chartView.Chart = barchart;
 
         }
-       
+
+        private void DrawChartForTransactionsPending()
+        {
+            List<ChartEntry> entries = new List<ChartEntry>();
+            foreach (var d in InitialData.TotalTransactionByTime)
+            {
+                var random = new Random();
+                var color = String.Format("#{0:X6}", random.Next(0x1000000)); // = "#A197B9"
+                entries.Add(new ChartEntry(Convert.ToInt64(d.Count))
+                {
+                    Label = d.Key,
+                    ValueLabel = d.Count.ToString(),
+                    Color = SKColor.Parse(color)
+                });
+            }
+
+            var linechart = new LineChart { Entries = entries, LabelTextSize = 30f };
+            chartView5.Chart = linechart;
+
+        }
     }
 }
